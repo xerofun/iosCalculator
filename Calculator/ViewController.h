636649b3c5e4c2c53efc103975fa42430edd69e9
Brian Rogers <brian@Brian-Rogerss-Mac-mini.local>
2013-06-21 11:38:13 -0400

    Initial commit

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -9,5 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *calculatorDisplay;
+@property (nonatomic) int first;
+@property (nonatomic) int second;
+@property (nonatomic) char operation;
+@property (nonatomic) BOOL isTyping;
+
+- (IBAction)numberPressed:(UIButton *)sender;
+- (IBAction)operationPressed:(UIButton *)sender;
+- (IBAction)equalsPressed:(UIButton *)sender;
 
 @end
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -26,4 +26,51 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)numberPressed:(UIButton *)sender
+{
+    NSString *number = sender.currentTitle;
+    if (self.isTyping)
+    {
+        self.calculatorDisplay.text = [self.calculatorDisplay.text stringByAppendingString:number];
+    }
+    else
+    {
+        self.calculatorDisplay.text = number;
+        self.isTyping = true;
+    }
+}
+
+- (IBAction)operationPressed:(UIButton *)sender
+{
+    // Store current number as first number
+    self.first = [self.calculatorDisplay.text intValue];
+    self.operation = [sender.currentTitle characterAtIndex:0];
+    self.isTyping = false;
+}
+
+- (IBAction)equalsPressed:(UIButton *)sender
+{
+    double answer;
+    self.second = [self.calculatorDisplay.text intValue];
+    switch (self.operation)
+    {
+        case '+':
+            answer = self.first + self.second;
+            break;
+        case '-':
+            answer = self.first - self.second;
+            break;
+        case '/':
+            // TODO: Watch for divide by zero
+            answer = self.first / self.second;
+            break;
+        case '*':
+            answer = self.first * self.second;
+            break;
+    }
+    
+    self.isTyping = false;
+    self.calculatorDisplay.text = [NSString stringWithFormat:@"%f", answer];
+}
+
 @end
